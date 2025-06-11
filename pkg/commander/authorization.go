a06e3fc36b80c277cb2051c6e7297f4a9711b03c
Nay Min Ko <developer@nayminko.one>
2025-06-11 12:32:26 +0200

    Used separated error codes for authorization; added HTTP_AUTHORIZATION_KEY to standard deployment.

--- pkg/commander/authorization.go
+++ pkg/commander/authorization.go
@@ -10,9 +10,12 @@ import (
 
 func CheckAuthorization(ctx *gin.Context, cfg *config.Config) error {
 	authorizationHeader := ctx.Request.Header.Get("Authorization")
-	if authorizationHeader == "" || authorizationHeader != "Bearer "+cfg.Settings.HttpAuthorizationKey {
+	if authorizationHeader == "" {
 		ctx.String(http.StatusUnauthorized, "Unauthorized")
 		return errors.New("unauthorized")
+	} else if authorizationHeader != "Bearer "+cfg.Settings.HttpAuthorizationKey {
+		ctx.String(http.StatusForbidden, "Forbidden")
+		return errors.New("incorrect authorization")
 	}
 
 	return nil
--- pkg/commander/routes.go
+++ pkg/commander/routes.go
@@ -3,6 +3,7 @@ package commander
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/iLert/ilert-kube-agent/pkg/config"
+	"github.com/rs/zerolog/log"
 )
 
 func SetUpMcpRoutes(router *gin.Engine, cfg *config.Config) {
@@ -18,6 +19,7 @@ func SetUpMcpRoutes(router *gin.Engine, cfg *config.Config) {
 func AuthorizedHandler(cfg *config.Config, handler func(*gin.Context, *config.Config)) func(*gin.Context) {
 	return func(ctx *gin.Context) {
 		if err := CheckAuthorization(ctx, cfg); err != nil {
+			log.Warn().Err(err).Msg("Authorization failed")
 			return
 		}
 		handler(ctx, cfg)
